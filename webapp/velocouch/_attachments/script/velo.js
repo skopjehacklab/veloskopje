7ca31e959be02f9fb93ab0dda78206631daea5ee
Vladan Popovic <vladanovic@gmail.com>
2012-05-13 03:24:30 +0200

    use jquery's methodts to add html elements

--- webapp/velocouch/_attachments/script/velo.js
+++ webapp/velocouch/_attachments/script/velo.js
@@ -1,10 +1,23 @@
+function formatDate(dt) {
+    date = dt.getDate() + "." + parseInt(dt.getMonth()+1) + "." + dt.getFullYear();
+    time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    return date + " " + time;
+}
+
 jQuery(function($) {
     $Couch.view('reports', {}).done(
         function(data) {
             for (var i = 0; i < data.rows.length; i++) {
-                var report = $("<div>").html(data.rows[i].value.timestamp + "<br />" +
-                    data.rows[i].value.latitude + "-:-" + data.rows[i].value.longitude + "<br />" + "<img src='data:image/jpg;base64," + data.rows[i].value.image + "' /><br />" + data.rows[i].value.comment + "</div>");
-                $('#container').append(report);
+                var values = data.rows[i].value;
+                var c = $("<div>");
+                c.appendTo($("#container"));
+
+
+                c.append($("<p>").text(formatDate((new Date(values.timestamp)))));
+                c.append($("<p>").text(values.comment).appendTo(c));
+                c.append($("<img>").attr("src", "data:image/jpg;base64," + values.image));
+                c.append($("<a>").attr("href", "http://www.openstreetmap.org/?lat=" + values.latitude +
+                        "&lon=" + values.longitude + "&zoom=17&layers=M").text("Види на мапа"));
             }
         });
 })
