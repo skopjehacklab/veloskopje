10fe736e0b3a6d58ca51e7e8503a79b273bdc328
Aleksandar Balalovski <gemidjy@gemidjy-debilian.(none)>
2012-05-15 00:48:46 +0200

    Web upload to couchdb works. For now. Latest apk in dist/

--- mobile/android/src/com/kika/veloskopje/activities/WebReportActivity.java
+++ mobile/android/src/com/kika/veloskopje/activities/WebReportActivity.java
@@ -1,17 +1,17 @@
 package com.kika.veloskopje.activities;
 
 
-import java.io.UnsupportedEncodingException;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.util.Date;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
-import org.json.JSONException;
-import org.json.JSONStringer;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -25,6 +25,7 @@ import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -35,7 +36,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kika.veloskopje.R;
-import com.kika.veloskopje.helpers.HttpUtils;
 import com.kika.veloskopje.utils.Constants;
 import com.kika.veloskopje.utils.Utils;
 
@@ -82,7 +82,7 @@ public class WebReportActivity extends Activity {
 		mDiscardButton.setOnClickListener(mButtonClickListener);
 
 		mTimeTextview.setText(getTime());
-		
+
 		mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
 		Criteria criteria = new Criteria();
@@ -104,61 +104,68 @@ public class WebReportActivity extends Activity {
 		super.onResume();
 	}
 
-	public void publish() {
+	private void publish() {
+		
 		final ProgressDialog pD = ProgressDialog.show(this, "Пачекајте", "Податоците се испраќаат...");
-
-		new AsyncTask<Void, Void, Void>() {
+		new AsyncTask<Void, Void, Boolean>() {
 
 			@Override
-			protected Void doInBackground(Void... params) {
-				HttpPost request = new HttpPost(Constants.WEB_REPORT_URL);
-				String imgBase64 = Base64.encodeToString(Utils.getImageAsByteArray(mPhotoFileUri), Base64.DEFAULT);
-
-				String comment = "" + mInfoEditText.getText().toString();
-				JSONStringer json = null;
+			protected Boolean doInBackground(Void... arg0) {
+				HttpClient client = new DefaultHttpClient();
+				HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000);
+				HttpResponse response;
+				JSONObject json = new JSONObject();
 				try {
-					json = new JSONStringer()
-					.object()
-					.key("comment").value(comment)
-					.key("image").value(imgBase64)
-					.key("lat").value("42.60")
-					.key("lon").value("22.3")
-					.key("timestamp").value(System.currentTimeMillis())
-					.endObject();
-				} catch (JSONException e2) {
-					e2.printStackTrace();
-				}
+					HttpPost post = new HttpPost(Constants.WEB_REPORT_URL);
+					String imgBase64 = Base64.encodeToString(Utils.getImageAsByteArray(mPhotoFileUri), Base64.NO_WRAP);
+					
+					String comment = mInfoEditText.getText().toString();
+					if(comment==null) comment = "n/a";
+					json.put("image", imgBase64);
+					json.put("comment", comment);
+					json.put("latitude", String.valueOf(mLat));
+					json.put("longitude", String.valueOf(mLon));
+					json.put("timestamp", System.currentTimeMillis());
 
-				StringEntity entity = null;
-				try {
-					entity = new StringEntity(json.toString());
-				} catch (UnsupportedEncodingException e1) {
-					e1.printStackTrace();
-				}
-				entity.setContentType("application/json;charset=UTF-8");
-				entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));
-				entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_LEN, String.valueOf(json.toString().length())));
+//					post.setHeader("json", json.toString());
+					
+					StringEntity se = new StringEntity(json.toString());
+					se.setContentEncoding("application/json;charset=UTF-8");
+					post.setEntity(se);
 
-				request.setHeader("Content-Range", "10" + Integer.valueOf(json.toString().length()-1));
-				request.setHeader("Accept-Ranges", "bytes");
+					post.addHeader("Content-Type", "application/json");
+					post.addHeader("Content-Lenght", String.valueOf(json.toString().length()));
+					
+//					Header[] headers = post.getAllHeaders();
+//					System.out.println(headers.toString());
 
-				request.setEntity(entity);
+					response = client.execute(post);
 
-				HttpResponse response = null;
-				try {
-					response = HttpUtils.getNewHttpClient().execute(request);
+					if (response != null) {
+						InputStream in = response.getEntity().getContent(); 
+						String a = Utils.convertStreamToString(in);
+						Log.i(Constants.TAG, a);
+						return true;
+					}
 				} catch (Exception e) {
-					e.printStackTrace();
-				} 
-
-				return null;
+					Log.e(Constants.TAG, "Exception", e);
+					return false;
+				}
+				
+				return false;
 			}
-
+			
 			@Override
-			public void onPostExecute(Void result) {
+			public void onPostExecute(Boolean result) {
+				String statusMsg = "Се случи грешка при испраќањето. Обидете се повторно."; 
+				if(result) {
+					statusMsg = "Вашата слика е успешно испратена.";
+					finish();
+				}
+				
+				Toast.makeText(WebReportActivity.this, statusMsg, Toast.LENGTH_LONG).show();
 				pD.dismiss();
 			}
-
 		}.execute();
 	}
 
@@ -190,7 +197,7 @@ public class WebReportActivity extends Activity {
 		public void onLocationChanged(Location location) {
 			mLat = location.getLatitude();
 			mLon = location.getLongitude();
-			
+
 			String openStreetMapUrl = String.format("http://www.openstreetmap.org/?lat=%f&lon=%f&zoom=15&layers=B000FTF", mLat, mLon);
 			mLocationTextview.setText(openStreetMapUrl);
 			Linkify.addLinks(mLocationTextview, Linkify.ALL);
